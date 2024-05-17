7fb0380077ee0b5c45b7b2a5fec9d79e9ca17c69
agrathwohl <andrew@grathwohl.me>
2024-05-17 12:39:01 -0500

    example

--- playground/app.vue
+++ playground/app.vue
@@ -19,29 +19,35 @@
   const { $locally } = useNuxtApp()
   const srcRef = ref('')
   try {
+    // Login!
     const oauthStuff = await usePeertubeClient()
     $locally.setItem('access_token', oauthStuff.access_token)
     console.log($locally.getItem('access_token'), ' set access token in local storage ')
 
+    // Get user history of logged in user!
     const history = await usePeertubeUserHistory(oauthStuff)
     console.log(history, 'HISTORY')
 
+    // Get a peertube channel!
     const channel = await usePeertubeChannel('part.of.the.problem', oauthStuff.access_token)
     console.log(channel)
 
+    // Get a peertube channel's videos!
     const videosFromChannel = channel.recentVideos(20)
     const videoFromChannel = videosFromChannel[11]
 
 
+    // Perform a search on a peertube!
     const searchTerm = 'war'
-
     const searchResults = await usePeertubeSearch(searchTerm, 20)
     console.log(searchResults)
 
+    // Get a video!
     const vid = await usePeertubeVideo(videoFromChannel.id)
     console.log(vid)
     srcRef.value = `https://gas.tube.sh${vid.embedPath}?api=1`
 
+    // Play a video!
     if (typeof window !== '') {
       console.log('WINDOW!!')
 
